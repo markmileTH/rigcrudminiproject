99acf2a0573bff3ae8ed9f2b2421f9ec300c577a
4NUB!S <xexjz0leechersteamofficialclub@gmail.com>
2022-03-01 23:45:01 +0700

    update

--- index.js
+++ index.js
@@ -129,46 +129,46 @@ router.put('/user', (req, res) => {
           res.status(400).send({ error: true, message: "กรุณากรอก phoneNumber" })
      }
      else if (username.length < 8) {
-     res.status(400).send({ error: true, message: "กรุณากรอก Username มากกว่า 8 ตัว" })
+          res.status(400).send({ error: true, message: "กรุณากรอก Username มากกว่า 8 ตัว" })
      } else if (password.search(/^(?=.*[a-z])(?=.*[A-Z]).{8,}$/)) {
-     res.status(400).send({ error: true, message: "กรุณากรอก Password ให้ครบถ้วน (มากกว่า 8 ตัว และอักษร a-z , A-Z)" })
+          res.status(400).send({ error: true, message: "กรุณากรอก Password ให้ครบถ้วน (มากกว่า 8 ตัว และอักษร a-z , A-Z)" })
      } else if (gmail.search(/^(?=.*[@])/)) {
-     res.status(400).send({ error: true, message: "กรุณากรอก Gmail ให้ถูกต้อง" })
+          res.status(400).send({ error: true, message: "กรุณากรอก Gmail ให้ถูกต้อง" })
      } else if (phoneNumber.search(/^(?=.*[0-9])(?!.*[!@#$%^&*])(?!.*[a-zA-Z])/) || phoneNumber.length != 10) {
-     res.status(400).send({ error: true, message: "กรุณากรอก phoneNumber ให้ถูกต้อง( 0-9 )" })
-     }else{
-     let sql = "UPDATE user SET username =?, password =? ,gmail =?, phoneNumber=? WHERE userID =? "
-     db.query(sql, [username, password, gmail, phoneNumber, userID], (error, results, fields) => {
-          if (error) throw error;
-          let message = ""
-          if (results.changedRows === 0) {
-               message = " not found or data is same"
-          } else {
-               message = "Successfully updated user data"
-          }
-          res.send({ error: false, data: results, message: message })
-     })
-}
- })
+          res.status(400).send({ error: true, message: "กรุณากรอก phoneNumber ให้ถูกต้อง( 0-9 )" })
+     } else {
+          let sql = "UPDATE user SET username =?, password =? ,gmail =?, phoneNumber=? WHERE userID =? "
+          db.query(sql, [username, password, gmail, phoneNumber, userID], (error, results, fields) => {
+               if (error) throw error;
+               let message = ""
+               if (results.changedRows === 0) {
+                    message = " not found or data is same"
+               } else {
+                    message = "Successfully updated user data"
+               }
+               res.send({ error: false, data: results, message: message })
+          })
+     }
+})
 
- router.delete('/delete', (req, res) => {
-      let userID = req.body.userID;
-      if (!userID) {
-           res.status(400).send({ error: true, message: "Please provide user id" })
-      } else {
-           let sql = "DELETE FROM user WHERE userID =? "
-           db.query(sql, [userID], (error, results, fields) => {
-                if (error) throw error;
-                let message = ""
-                if (results.affectedRows === 0) {
-                     message = "User not found."
-                } else {
-                     message = "Successfully deleted user data."
-                }
-                res.status(202).send({ error: false, data: results, message: message })
-           })
-      }
- })
+router.delete('/delete', (req, res) => {
+     let userID = req.body.userID;
+     if (!userID) {
+          res.status(400).send({ error: true, message: "Please provide user id" })
+     } else {
+          let sql = "DELETE FROM user WHERE userID =? "
+          db.query(sql, [userID], (error, results, fields) => {
+               if (error) throw error;
+               let message = ""
+               if (results.affectedRows === 0) {
+                    message = "User not found."
+               } else {
+                    message = "Successfully deleted user data."
+               }
+               res.status(202).send({ error: false, data: results, message: message })
+          })
+     }
+})
 
 app.use(router)
 app.listen(port, () => {
