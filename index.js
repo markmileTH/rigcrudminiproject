88df0c18bd4422ba31d645133fd0bd54671bdbe8
4NUB!S <xexjz0leechersteamofficialclub@gmail.com>
2022-03-01 13:12:10 +0700

    update

--- index.js
+++ index.js
@@ -30,26 +30,25 @@ db.connect((err) => {
      console.log("MySql Connected");
 });
 
-// router.get('/', (req, res) => {
-//      res.send({
-//           error: false,
-//           message: "Welcom to RESTful API Coffiemade with NodeJS.",
-//           writen_by: "thanakhan"
-//      })
-// })
+ router.get('/', (req, res) => {
+      res.send({
+           error: false,
+           message: "CoffeeMadeShop."
+      })
+ })
 
-// router.get('/user', (req, res) => {
-//      db.query('SELECT * FROM user', (error, results, fields) => {
-//           if (error) throw error;
-//           let message = ""
-//           if (results === undefined || results.length == 0) {
-//                message = "pharmacy table is empty"
-//           } else {
-//                message = "Successfuly retrieved all user"
-//           }
-//           res.send({ error: false, data: results, massage: message })
-//      })
-// })
+ router.get('/user', (req, res) => {
+      db.query('SELECT * FROM user', (error, results, fields) => {
+           if (error) throw error;
+           let message = ""
+           if (results === undefined || results.length == 0) {
+                message = "pharmacy table is empty"
+           } else {
+                message = "Successfuly retrieved all user"
+           }
+           res.send({ error: false, data: results, massage: message })
+      })
+ })
 
 router.post('/user', (req, res) => {
      let username = req.body.username;
@@ -88,49 +87,47 @@ router.post('/user', (req, res) => {
      }
 })
 
-// router.get('/user/:id', (req, res) => {
-//      let id = req.params.id;
-//      if (!id) {
-//           res.status(400).send({ error: true, message: "Please provide user id." })
-//      } else {
-//           let sql = "SELECT * FROM user where userID = ?"
-//           db.query(sql, id, (error, results, fields) => {
-//                let message = ""
-//                if (results === undefined || results.length == 0) {
-//                     message = "user not found"
-//                     res.send({ error: false, data: results, message: message })
-//                } else {
-//                     message = "Successfully retieved user data"
-//                     res.send({ error: false, data: results[0], message: message })
-//                }
-
-//           })
-//      }
-// })
-
-// router.put('/user', (req, res) => {
-//      let userID = req.body.userID;
-//      let username = req.body.username;
-//      let password = req.body.password;
-//      let gmail = req.body.gmail;
-//      let phoneNumber = req.body.phoneNumber
-
-//      if (!userID || !username || !password || !gmail ||!phoneNumber ) {
-//           res.status(400).send({ error: true, message: "Please provide idPharmacy,PharmacyName,PharmacyType,Price " })
-//      } else {
-//           let sql = "UPDATE user SET username =?, password =? ,gmail =?, phoneNumber WHERE userID =? "
-//           db.query(sql, [username,password,gmail,phoneNumber,userID], (error, results, fields) => {
-//                if (error) throw error;
-//                let message = ""
-//                if (results.changedRows === 0) {
-//                     message = " not found or data is same"
-//                } else {
-//                     message = "Successfully updated user data"
-//                }
-//                res.send({ error: false, data: results, message: message })
-//           })
-//      }
-// })
+ router.get('/user/:id', (req, res) => {
+      let id = req.params.id;
+      if (!id) {
+           res.status(400).send({ error: true, message: "Please provide user id." })
+      } else {
+           let sql = "SELECT * FROM user where userID = ?"
+           db.query(sql, id, (error, results, fields) => {
+                let message = ""
+                if (results === undefined || results.length == 0) {
+                     message = "user not found"
+                     res.send({ error: false, data: results, message: message })
+                } else {
+                     message = "Successfully retieved user data"
+                     res.send({ error: false, data: results[0], message: message })
+                
+           }})
+      }
+ })
+// FIX
+ router.put('/user', (req, res) => {
+      let userID = req.body.userID;
+      let username = req.body.username;
+      let password = req.body.password;
+      let gmail = req.body.gmail;
+      let phoneNumber = req.body.phoneNumbe
+      if (!userID || !username || !password || !gmail ||!phoneNumber ) {
+           res.status(400).send({ error: true, message: "Please provide userID,username,password,gmail,phoneNumber " })
+      } else {
+           let sql = "UPDATE user SET username =?, password =? ,gmail =?, phoneNumber WHERE userID =? "
+           db.query(sql, [username,password,gmail,phoneNumber,userID], (error, results, fields) => {
+                if (error) throw error;
+                let message = ""
+                if (results.changedRows === 0) {
+                     message = " not found or data is same"
+                } else {
+                     message = "Successfully updated user data"
+                }
+                res.send({ error: false, data: results, message: message })
+           })
+      }
+ })
 
 // router.delete('/delete', (req, res) => {
 //      let userID = req.body.userID;
